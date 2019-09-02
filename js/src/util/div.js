4ffd542aa2f95c23db0923b54731b7e517f1cd7e
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-09-02 14:45:55 +0200

    Fix comment in div func

--- js/src/util/div.js
+++ js/src/util/div.js
@@ -2,9 +2,8 @@ import h from './h';
 
 /**
  * utility function to return empty div
- * @return {HTMLElement}
+ * @return {Node}
  */
-
 const div = () => { return h('div') };
 
 export default div;
